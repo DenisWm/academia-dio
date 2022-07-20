940e42b403be80b881e84d5bab08f7e5b126da6b
DenisWm <denismamoni@hotmail.com>
2022-07-20 09:34:34 -0400

    Anotando as entidades aluno, matricula e avaliação_fisica

---
+++ src/main/java/me/dio/academia/academia/digital/model/Matricula.java
@@ -0,0 +1,28 @@
+package me.dio.academia.academia.digital.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_matriculas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Matricula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+    private LocalDateTime dataDaMatricula = LocalDateTime.now();
+
+}
